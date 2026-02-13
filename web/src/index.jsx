7d0aad3391004a3f627c91b9804cd1ec80822d4a
LEGEND <risinglegendnzl@gmail.com>
2026-02-14 00:25:37 +1100

    Update index.jsx

--- web/src/index.jsx
+++ web/src/index.jsx
@@ -9,6 +9,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { ThemeProvider } from './contexts/ThemeContext';
+import './cheffy-sticky-tabs-enhancements.css';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
