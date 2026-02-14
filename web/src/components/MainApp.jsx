6f2b67cfa8a73fe040370bde04947da2a75feb01
LEGEND <risinglegendnzl@gmail.com>
2026-02-15 02:21:28 +1100

    Update MainApp.jsx

--- web/src/components/MainApp.jsx
+++ web/src/components/MainApp.jsx
@@ -154,7 +154,7 @@ const MainApp = ({
     handleFetchNutrition,
     handleSubstituteSelection,
     handleQuantityChange,
-    handleDownloadMatchTraceReport,,
+    handleDownloadMatchTraceReport,
     handleDownloadLogs,
     onToggleMealEaten,
     handleRefresh,
